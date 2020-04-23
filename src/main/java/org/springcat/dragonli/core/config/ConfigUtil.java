5eeab65f1fee4f98ede69579355e919d7d4f30ab
chenzhenwei <15868107710@139.com>
2020-04-23 18:22:08 +0800

    重构代码结构

--- src/main/java/org/springcat/dragonli/core/config/ConfigUtil.java
+++ src/main/java/org/springcat/dragonli/core/config/ConfigUtil.java
@@ -3,6 +3,8 @@ package org.springcat.dragonli.core.config;
 import cn.hutool.cache.CacheUtil;
 import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.lang.Dict;
+import cn.hutool.setting.Setting;
+import cn.hutool.setting.SettingUtil;
 import com.ecwid.consul.v1.Response;
 import com.ecwid.consul.v1.kv.model.GetValue;
 import org.springcat.dragonli.core.registry.AppInfo;
@@ -10,6 +12,11 @@ import org.springcat.dragonli.core.consul.ConsulUtil;
 
 import java.util.List;
 
+/**
+ * sysconf 用于系统启动前获取的参数,比如数据库配置等
+ * userconf 用于普通的业务参数
+ * 后续会把配置异步写入本地文件,遇到配置中心异常后,可以不影响业务运行
+ */
 public class ConfigUtil {
 
     private static TimedCache<String, String> userConfCache = CacheUtil.newTimedCache(1000);
